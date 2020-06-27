81e5fc61a48c06f12cca8604241d97bbed09325e
Masashi Okita <easychoco2@gmail.com>
2020-06-28 01:24:46 +0900

    template更新

--- AtCoder/ABC999/A.cpp
+++ AtCoder/ABC999/A.cpp
@@ -2,12 +2,12 @@
 
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define endl "\n"
-#define PI 3.14159265358979
-#define MOD 1000000007 // = 10^9 + 7
 
 using namespace std;
 using ll = long long;
 
+const ll MOD = 1000000007LL; // = 10^9 + 7
+const double PI = 3.14159265358979;
 
 void solve()
 {
