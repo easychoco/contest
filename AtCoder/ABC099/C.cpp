c1a88eb09fd9dcf9d4faa34c08cdf53c60b1c010
Masashi Okita <easychoco2@gmail.com>
2020-08-04 08:09:13 +0900

    ABC099-C

--- AtCoder/ABC099/C.cpp
+++ AtCoder/ABC099/C.cpp
@@ -6,12 +6,45 @@
 using namespace std;
 using ll = long long;
 
-const ll MOD = 1000000007LL; // = 10^9 + 7
-const double PI = 3.14159265358979;
-
+//15min
 void solve()
 {
-  
+  int n;
+  cin >> n;
+
+  const ll INF = 1LL << 60;
+  ll dp[100010];  
+  fill(dp, dp + 100010, INF);
+  dp[0] = 0;
+
+  vector<int> yen { 1 };
+
+  int num = 6;
+  while(num <= n)
+  {
+    yen.emplace_back(num);
+    num *= 6;
+  }
+
+  num = 9;
+  while(num <= n)
+  {
+    yen.emplace_back(num);
+    num *= 9;
+  }
+
+  sort(yen.rbegin(), yen.rend());
+
+  for(int i = 0; i <= n; ++i)
+  {
+    for(const auto& y : yen)
+    {
+      if (i + y > n) continue;
+      dp[i + y] = min(dp[i + y], dp[i] + 1);
+    }
+  }
+
+  cout << dp[n];
 }
 
 int main()
