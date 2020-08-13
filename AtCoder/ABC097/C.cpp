9c1b75c5aa4ba6a74c5a0033706e0e0c5d9e3ccd
Masashi Okita <easychoco2@gmail.com>
2020-08-13 10:53:13 +0900

    ABC097-C

--- AtCoder/ABC097/C.cpp
+++ AtCoder/ABC097/C.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
 
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define endl "\n"
@@ -11,7 +11,40 @@ const double PI = 3.14159265358979;
 
 void solve()
 {
-  
+  string s;
+  int k;
+  cin >> s >> k;
+  set<char> ana;
+  for(const char& c : s) ana.insert(c);
+
+  vector<string> subset;
+  for(const char c : ana)
+  {
+    for(int i = 0; i < s.length(); ++i)
+    {
+      if (s[i] == c)
+      {
+        for(int j = 1; i + j <= min((int)s.length(), i + k); ++j)
+        {
+          subset.emplace_back(s.substr(i, j));
+        }
+      }
+    }
+    if (subset.size() >= k)
+    {
+      sort(subset.begin(), subset.end());
+      subset.erase( unique(subset.begin(), subset.end()), subset.end() );
+
+      if (subset.size() >= k)
+      {
+        cout << subset[k - 1];
+        return;
+      }
+    }
+  }
+
+
+
 }
 
 int main()
