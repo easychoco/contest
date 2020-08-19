e25f691f1f579415635c94e88b2540739ce8783d
Masashi Okita <easychoco2@gmail.com>
2020-08-19 17:27:58 +0900

    ABC095-C

--- AtCoder/ABC095/C.cpp
+++ AtCoder/ABC095/C.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <iostream>
 
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define endl "\n"
@@ -11,7 +12,21 @@ const double PI = 3.14159265358979;
 
 void solve()
 {
+  int a, b, c, x, y;
+  cin >> a >> b >> c >> x >> y;
   
+  int ans = a * x + b * y;
+  ans = min( ans, c * 2 * max(x, y) );
+
+  if (x < y)
+  {
+    ans = min( ans, x * 2 * c + (y - x) * b );
+  }
+  else
+  {
+    ans = min( ans, y * 2 * c + (x - y) * a );
+  }
+  cout << ans;
 }
 
 int main()
