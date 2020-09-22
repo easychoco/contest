9019a85d3d53f3b555090a9e6c430feffd2f248d
Masashi Okita <easychoco2@gmail.com>
2020-09-22 13:36:09 +0100

    ABC071

---
+++ AtCoder/ABC071/D.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+
+using namespace std;
+using ll = long long;
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+const double PI = 3.14159265358979;
+
+void solve()
+{
+  int n;
+  string s1, s2;
+  cin >> n >> s1 >> s2;
+
+  bool type = (s1[0] == s2[0]);
+  mint ans = type ? 3 : 6;
+  for(int i = 1 + (!type); i < n; ++i)
+  {
+    // 縦向き
+    if (s1[i] == s2[i])
+    {
+      ans *= type ? 2 : 1;
+      type = true;
+    }
+    else
+    {
+      ans *= type ? 2 : 3;
+      type = false;
+      i++;
+    }
+  }
+  cout << ans;
+}
+
+int main()
+{
+  fastio;
+  solve();
+
+  return 0;
+}
