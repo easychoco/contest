605a23151ff253d41a63cf4b219583f04350bb4f
Masashi Okita <easychoco2@gmail.com>
2020-10-01 04:04:20 +0100

    ABC175-E

--- AtCoder/ABC175/E.cpp
+++ AtCoder/ABC175/E.cpp
@@ -10,39 +10,57 @@ const ll MOD = 1000000007LL; // = 10^9 + 7
 const double PI = 3.14159265358979;
 
 // score[行][列][行内での個数]
-ll score[3010][3010][3];
+ll dp[3010][3010][4];
 ll value[3010][3010];
-
 
 void solve()
 {
-  ll r, c, k, inr, inc, inv;
-  cin >> r >> c >> k;
+  ll Row, Col, k, r, c, v;
+  cin >> Row >> Col >> k;
   for(ll i = 0; i < k; ++i)
   {
-    cin >> inr >> inc >> inv;
-    value[inr][inc] = inv;
+    cin >> r >> c >> v;
+    r--; c--;
+    value[r][c] = v;
   }
 
-  for(ll row = 1; row <= r; ++row)
+  for(int i = 0; i < Row; ++i)
+    for(int j = 0; j < Col; ++j)
+      for(int k = 0; k < 4; ++k)
+        dp[i][j][k] = -10;
+
+  dp[0][0][0] = 0;
+  for(int i = 0; i < Row; ++i)
   {
-    for(ll col = 1; col <= c; ++col)
+    for(int j = 0; j < Col; ++j)
     {
-      score[row][col][0] = max(score[row][col - 1][0], 
-        max({
-          score[row - 1][col][0],
-          score[row - 1][col][1],
-          score[row - 1][col][2]
-        })
-        + value[row][col]
-      );
-      score[row][col][1] = max(score[row][col][1], score[row][col - 1][0] + value[row][col]);
-      score[row][col][2] = max(score[row][col][2], score[row][col - 1][1] + value[row][col]);
-      // cout << row << " " << col << " " << score[row][col][0] << endl;
+      if (dp[i][j][2] >= 0) dp[i][j][3] = max(dp[i][j][3], dp[i][j][2] + value[i][j]);
+      if (dp[i][j][1] >= 0) dp[i][j][2] = max(dp[i][j][2], dp[i][j][1] + value[i][j]);
+      if (dp[i][j][0] >= 0) dp[i][j][1] = max(dp[i][j][1], dp[i][j][0] + value[i][j]);
+
+      for(int k = 0; k < 4; ++k)
+      {
+        if(dp[i][j][k] >= 0)
+        {
+          if (i + 1 < Row)
+          {
+            dp[i + 1][j][0] = max(dp[i + 1][j][0], dp[i][j][k]);
+          }
+          if (j + 1 < Col)
+          {
+            dp[i][j + 1][k] = max(dp[i][j + 1][k], dp[i][j][k]);
+          }
+        }
+      }
     }
   }
-
-  cout << max( { score[r][c][0], score[r][c][1], score[r][c][2] } );
+  
+  cout << max({
+    dp[Row - 1][Col - 1][0], 
+    dp[Row - 1][Col - 1][1], 
+    dp[Row - 1][Col - 1][2], 
+    dp[Row - 1][Col - 1][3]
+  });
 }
 
 int main()
