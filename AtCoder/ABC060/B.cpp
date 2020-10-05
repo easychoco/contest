8fb2fa89af7a5a9229d071da0d85c2debfff649a
Masashi Okita <easychoco2@gmail.com>
2020-10-05 14:27:57 +0100

    ABC060

---
+++ AtCoder/ABC060/B.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i = a;i < (ll)n;++i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define SHOW(str,val) cout<<(str)<<" : "<<(val)<<endl;
+
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+// using P = pair<int, int>;
+void YN(bool a) { cout << (a ? "YES" : "NO"); }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true;} return false;}
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true;} return false;}
+
+void solve()
+{
+  int a, b, c;
+  cin >> a >> b >> c;
+  bool y = false;
+  for(int i = 1; i <= 100; ++i)
+  {
+    y |= ((a * i % b) == c);
+  }
+  YN(y);
+}
+
+int main()
+{
+  fastio;
+  solve();
+
+  return 0;
+}
