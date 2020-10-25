2a1eb804f7283b846b828f53aa0cda7a7c42ed1c
Masashi Okita <easychoco2@gmail.com>
2020-10-25 12:00:09 +0100

    AOJフォルダのtipsとtemplateを変更

--- AOJ/_tips.cpp
+++ AOJ/_tips.cpp
@@ -4,13 +4,22 @@
 // --std=c++14 -Wl,--stack,10485760 
 
 #include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=a;i<(ll)n;++i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define SHOW(str,val) cout<<(str)<<" : "<<(val)<<endl
+
 using namespace std;
-
-#define PI 3.14159265358979
-
-
 using ll = long long;
-using P = pair<int, int>;
+using P = pair<ll, ll>;
+// using P = pair<int, int>;
+void YN(bool a) { cout << (a ? "Yes" : "No"); }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true;} return false;}
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true;} return false;}
 
 // 最大公約数 : 3,4 ->  1
 ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
@@ -49,22 +58,12 @@ ll combmod(ll n, ll r, ll mod) {
 
 // =================================================================================================== //
 
-void tip_algorithm()
-{
-  auto vec = vector<int> { 0, 1, 2 };
-  int x = 4, y = 5;
-
-  for(auto &v : vec) {}	// 走査
-
-  swap(x, y);				// 値を交換する
-}
-
 // 素因数分解
 void tip_facotization()
 {
   ll n;
   map<ll, ll> primes;
-  int factor = 2;
+  ll factor = 2;
   while(factor * factor <= n)
   {
     if (n % factor == 0)
@@ -77,7 +76,53 @@ void tip_facotization()
   primes[n]++;
 }
 
-// ワーシャルフロイド法・全点対最短経路 O(n^3)
+
+// クラスカル法
+// 最小全域木問題
+// [UnionFind木] を貼る
+void tip_kruskal()
+{
+  // ======================================
+  // このクラスはいらない
+  // コンパイルを通すためのダミークラス
+  class UnionFind{ 
+    public: UnionFind(ll a){}
+    bool same(ll a, ll b){ return true; }
+    void unite(ll a, ll b){}
+  };
+  // ここまでいらない
+  // ======================================
+
+  ll n = 1;
+  //     tuple<cost, from, to>
+  vector<tuple<ll, ll, ll> > G;
+  UnionFind uf(n);
+  
+  rep(i, n)
+  {
+    ll cost, from, to;
+    G.emplace_back(cost, from, to);
+  }
+  sort(ALL(G));
+
+  ll dist_sum = 0;
+  ll edge_cnt = 0;
+  for(const auto& edge : G)
+  {
+    ll cost, from, to;
+    tie(cost, from, to) = edge;
+    if (!uf.same(from, to))
+    {
+      uf.unite(from, to);
+      dist_sum += cost;
+      ++edge_cnt;
+    }
+  }
+}
+
+
+// ワーシャルフロイド法
+// 全点対最短経路 O(n^3)
 void tip_warshall_floyd(int n) {
   int d[300][300];
   for (int k = 0; k < n; k++){			// 経由する頂点
@@ -89,10 +134,16 @@ void tip_warshall_floyd(int n) {
   }
 }
 
-// Dijkstra法
+// Dijkstra法 ダイクストラ法
+// 単一始点最短経路問題 O(E log V)
 struct Edge{ ll cost, to; };
 void dijkstra()
 {
+  // 重みに負値があるときはベルマンフォード法を使う
+
+  // w=1(固定長)の時はpriority_queueをqueueにするとO(NlogN)がO(logN)になる
+  // 大抵は不要だけどO(NlogN)では厳しいときに
+  // 下のtip_graph_bfsも検討
   ll n, u, v, w;
   cin >> n;
   vector< vector<Edge> > G(n, vector<Edge>());
@@ -117,27 +168,130 @@ void dijkstra()
     if (d[node] < p.first) continue;
     for (const auto& edge : G[node])
     {
-        if (d[edge.to] > d[node] + edge.cost)
-        {
-            d[edge.to] = d[node] + edge.cost;
-            que.push(P(d[edge.to], edge.to));
-        }
+      if (d[edge.to] > d[node] + edge.cost)
+      {
+        d[edge.to] = d[node] + edge.cost;
+        que.push(P(d[edge.to], edge.to));
+      }
     }
   }
 }
 
+// BFSを使った無向グラフの全探索
+void tip_graph_bfs()
+{
+  ll n, m, a, b;
+  cin >> n >> m;
+  vector<P> input(m);
+  vector< vector<int> > edge(n, vector<int>());
+  for(int i = 0; i < m; ++i)
+  {
+    cin >> a >> b;
+    a--; b--;
+    input[i] = P(a, b);
+    edge[a].emplace_back(b);
+    edge[b].emplace_back(a);
+  }
+
+  // visited使用
+  vector<bool> visited(n, false);
+  queue<int> que;
+  que.push(0); //初期位置
+  while(!que.empty())
+  {
+    int now = que.front();
+    que.pop();
+    visited[now] = true;
+    for(const auto& to : edge[now])
+    {
+      if (!visited[to]) //visited使用
+        que.push(to);
+    }
+  }
+}
+
+// 二分探索
+// めぐる式
 void tip_binary_search()
 {
   auto f = [&](ll arg){ return arg; };
-  ll x;
-  ll le = 0, re = 1000000000LL;
-  while(re - le > 1)
+  ll ac = 0, wa = 1000000000LL;
+  while(wa - ac > 1)
   {
-    ll mid = (le + re) / 2;
-    if ( x < f(x) ) re = mid;
-    else le = mid;
+    ll wj = (ac + wa) / 2;
+    if ( !f(wj) ) wa = wj;
+    else ac = wj;
   }
 }
+
+// =================================================================================================== //
+
+// dynamic programming
+void tip_dp()
+{
+  /*
+  // knapsack
+  // ナップサック問題
+  // dp[i][j] = i個まで見て、重さがjの時の最大価値
+  vector< vector<ll> > dp(n + 1, vector<ll>(w + 1, 0));
+  rep (i, w) dp[0][i] = 0;
+  rep (i, n) rep (j, w)
+  {
+    if (j >= w[i]) dp[i + 1][j] = max(dp[i][j - w[i]] + v[i], dp[i][j]);
+    else dp[i+1][j] = dp[i][j];
+  }
+  cout << dp[n][w];
+
+  // knapsack
+  // ナップサック問題
+  // dp[i][j] = i個まで見て、重さがjの時の最大価値
+  vector< vector<ll> > dp(n + 1, vector<ll>(w + max_weight, 0));
+  rep(i, n) rep(x, w + 1)
+  {
+    chmax(dp[i + 1][x + w[i]], dp[i][x] + v[i]);
+    chmax(dp[i + 1][x], dp[i][x]);
+  }
+  cout << dp[n][w];
+
+  // knapsack
+  // ナップサック問題
+  const ll INF = 1LL << 60;
+  // dp[i][j] = i番目まで見て、価値がjの時の最小重さ
+  vector< vector<ll> > dp(n + 1, vector<ll>(Vsum + 1, INF));
+  dp[0][0] = 0;
+  rep(i, n) rep(j, Vsum)
+  {
+    if (j - v[i] >= 0) dp[i + 1][j] = min(dp[i][j - v[i]] + w[i], dp[i][j]);
+    else dp[i + 1][j] = dp[i][j];
+  }
+  ll ans = 0;
+  for(int i = Vsum; i >= 0; --i) {
+    if (dp[n][i] <= weight) {
+      ans = i;
+      break;
+    }
+  }
+
+  // tsp
+  // traveling salesman
+  // 巡回セールスマン問題
+  const ll INF = 1LL << 60;
+  // dp[i][j] = いまiにいて、j(mask)の街に訪れているときのコストの最小値
+  vector< vector<ll> > dp(1<<n, vector<ll>(n, INF));
+  repi(i, 1, n) dp[1<<i][i] = dist[0][i];
+  rep(mask, 1<<n) rep(now, n)
+  {
+    if (1 << now & mask) rep(next, n)
+    {
+      if (1 << next & mask) continue;
+      chmin(dp[mask | (1<<next)][next], dp[mask][now] + dist[now][next]);
+    }
+  }
+  cout << dp[(1 << n) - 1][0];
+  */
+}
+
+// =================================================================================================== //
 
 void tip_string()
 {
@@ -155,7 +309,7 @@ void tip_string()
 
 void tip_vector()
 {
-    vector<int> vec(5);
+  vector<int> vec(5);
 
   vec.size();			// vecの大きさ
   vec[3];				// i番目の要素にアクセス
@@ -278,6 +432,8 @@ void tip_unordered_set()
   s.clear();			// 空にする
 }
 
+// =================================================================================================== //
+
 class UnionFind{
 public:
   vector<int> p;		// 親
@@ -287,7 +443,7 @@ public:
     rank.resize(n, 1);
   }
   int root(int x){
-    if(p[x] == -1)return x;
+    if(p[x] == -1) return x;
     else return p[x] = root(p[x]); // 深さを 1 にしている
   }
   void unite(int x, int y){
@@ -297,17 +453,65 @@ public:
     rank[y] += rank[x];
     p[x] = y;
   }
+  // グループの数
+  ll root_num() {
+    ll num = 0;
+    for(ll x : p) if (x < 0) num++;
+    return num;
+  }
   //xが属すグループのサイズ
   int size(int x){ return rank[root(x)]; }
   bool same(int x, int y){ return (root(x) == root(y)); }
 };
 
+// Segment Tree
+// https://www.creativ.xyz/segment-tree-entrance-999/
+// 使用関数の引数は全て1-index
+// query() は半開区間
+// ===================  問題ごとに設定 ====================
+// !!使う前に init() を呼ぶ!!
+const int N = 1 << 20;              // 葉の数、2の累乗数
+const ll E = 0;                     // 演算での単位元
+ll op(ll a, ll b) { return a + b; } // 使用する演算、可換
+// =======================================================
+ll node[N * 2 + 1];
+void init(){
+  for(int i = 0; i < N * 2 + 1; ++i) node[i] = E;
+}
+ll get(int i) { return node[i + N - 1]; }
+void update(int i, ll x){ // i 番目の葉の値を x に変える
+  i += N - 1; // i 番目の葉のノード番号
+  node[i] = x;
+  while (i > 0) {
+    i = (i - 1) / 2; // ノード i の親ノードの番号に変える
+    node[i] = op(node[i * 2 + 1], node[i * 2 + 2]); // 左右の子の値を計算しなおす
+  }
+}
+ll query(int a, int b, int k = 0, int l = 0, int r = N){
+  // [a, b) の区間に対するクエリについてノード k （区間 [l, r) 担当）が答える
+  if (r <= a || b <= l) return E; // 区間が被らない場合は単位元を返す
+  if (a <= l && r <= b) return node[k]; // ノード k の担当範囲がクエリ区間 [a, b) に完全に含まれる
+  // 一部だけ範囲が被る場合は左右の子に委託する
+  ll c1 = query(a, b, 2 * k + 1, l, (l + r) / 2); // 左の子に値を聞く
+  ll c2 = query(a, b, 2 * k + 2, (l + r) / 2, r); // 右の子に値を聞く
+  return op(c1, c2);
+}
+// Segment Tree ここまで
+
 void solve()
 {
-  int n;
+  int x;
+  cin >> x;
+
+  bool y = x > 0;
+
+  YN(y);
+
+
+  ll n;
   cin >> n;
-  vector<int> a(n);
-  for(int i = 0; i < n; ++i)
+  vector<ll> a(n);
+  rep(i, n)
   {
     cin >> a[i];
   }
