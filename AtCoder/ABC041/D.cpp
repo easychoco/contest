0a62cb611299572e74d9d27a7270c70359d1ef25
Masashi Okita <easychoco2@gmail.com>
2020-10-26 05:34:20 +0100

    ABC041-D

--- AtCoder/ABC041/D.cpp
+++ AtCoder/ABC041/D.cpp
@@ -17,9 +17,35 @@ void YN(bool a) { cout << (a ? "Yes" : "No"); }
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
+int path[16][16];
+
 void solve()
 {
+  ll n, m, x, y;
+  cin >> n >> m;
+  vector<ll> dp(1<<n, 0);
+  rep(i, m)
+  {
+    cin >> x >> y;
+    --x; --y;
+    path[x][y] = 1;
+  }
 
+  dp[0] = 1;
+  rep(mask, 1<<n)
+  {
+    rep(i, n)
+    {
+      if (mask >> i & 1) continue;
+      bool ok = true;
+      rep (j, n)
+      {
+        if ((mask >> j & 1) && path[i][j]) ok = false;
+      }
+      if (ok) dp[mask | (1<<i)] += dp[mask];
+    }
+  }
+  cout << dp[(1<<n) - 1];
 }
 
 int main()
