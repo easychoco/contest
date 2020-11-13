32f578a40c4ec83a17570e5e9126575464c5d3be
Masashi Okita <easychoco2@gmail.com>
2020-11-13 08:44:50 +0000

    ABC168-E

--- AtCoder/ABC168/E.cpp
+++ AtCoder/ABC168/E.cpp
@@ -2,96 +2,108 @@
 
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define endl "\n"
-#define PI 3.14159265358979
-#define MOD 1000000007 // = 10^9 + 7
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=a;i<(ll)n;++i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define Show(val) cout<<(val)<<" "
+#define Showln(val) cout<<(val)<<endl
 
 using namespace std;
 using ll = long long;
+using P = pair<ll, ll>;
+// using P = pair<int, int>;
+void YN(bool a) { cout << (a ? "Yes" : "No"); }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+// const int mod = 998244353;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+// 最大公約数 : 3,4 ->  1
 ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
-
-ll powmod(ll a, ll n, ll mod)
-{
-	ll ret = 1;
-	while (n > 0)
-	{
-		if (n % 2 == 1)
-		{
-			ret = ret * a % mod;
-		}
-		a = a * a % mod;
-		n /= 2;
-	}
-	return ret;
-}
-
 
 void solve()
 {
-	int n;
-	cin >> n;
+  int n, a, b;
+  cin >> n;
 
-	map<pair<ll, ll>, ll> m;
+  ll zz = 0;
+  map<P, int> hist;
+  for(int i = 0; i < n; ++i)
+  {
+    cin >> a >> b;
 
-	for(int i = 0; i < n; ++i)
-	{
-		ll a, b;
-		cin >> a >> b;
+    // aは非負にする
+    if (a < 0){ a = -a; b = -b; }
 
-		ll gcd_ab = gcd(a, b);
+    if (a == 0 && b == 0) zz++;
+    else if (a == 0) hist[P(0, 1)]++;
+    else if (b == 0) hist[P(1, 0)]++;
+    else
+    {
+      ll cd = gcd(a, abs(b));
+      hist[P(a / cd, b / cd)]++;
+    }
+  }
 
-		pair<ll, ll> p = make_pair(a / gcd_ab, b / gcd_ab);
-		m[p]++;
-	}
+  mint ans = 1;
+  mint m2 = 2;
 
-	for(auto itr = m.begin(); itr != m.end(); ++itr)
-	{
-		cout << itr->first.first << " " << itr->first.second <<  " : " << itr->second << endl;
-	}
-	ll match = 0;
+  for(const auto& h : hist)
+  {
+    if (h.second == 0) continue;
+    P p = h.first;
+    P gemini = ((p.second < 0) ? P(-p.second, p.first) : P(p.second, -p.first) );
 
-	ll diff = 0;
+    ans *= (m2.pow(h.second) + m2.pow(hist[gemini]) - 1);
+    hist[h.first] = 0;
+    hist[gemini] = 0;
+  }
 
-	for(auto itr = m.begin(); itr != m.end(); ++itr)
-	{
-		pair<ll, ll> p1 = make_pair(-itr->first.second, itr->first.first);
-		if (m.count(p1) > 0)
-		{
-			match += m[p1];
-			diff += itr->second * m[p1];
-		}
-
-		pair<ll, ll> p2 = make_pair(itr->first.second, -itr->first.first);
-		if (m.count(p2) > 0)
-		{
-			match += m[p2];
-			diff += itr->second * m[p2];
-		}
-	}
-
-	ll ans = n;
-	ans += n * (n - 1) / 2;
-	ans -= diff / 2;
-	ans += match * match * (match - 1) / 2;
-
-	cout << diff << endl;
-	cout << match << endl;
-	cout << ans << endl;
-	// return;
-
-	ll non_match = n - match;
-
-	ans += (6 * powmod(2, non_match, MOD)) % MOD;
-	ans -= (1 + non_match + (non_match * (non_match - 1) / 2) % MOD);
-
-	cout << ans;
-
+  ans += zz - 1;
+  cout << ans << endl;
 }
 
 int main()
 {
-	fastio;
-	solve();
+  fastio;
+  solve();
 
-	return 0;
+  return 0;
 }
