72393f66eea7d7003273a5b37c794b1083d0472c
Masashi Okita <easychoco2@gmail.com>
2020-11-25 07:50:45 +0000

    ABC184-F

--- AtCoder/ABC184/F.cpp
+++ AtCoder/ABC184/F.cpp
@@ -19,13 +19,40 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true
 
 void solve()
 {
-  ll n;
-  cin >> n;
-  vector<ll> a(n);
-  rep(i, n)
+  ll n, t;
+  cin >> n >> t;
+  ll sz1 = n / 2, sz2 = n - sz1;
+  vector<ll> a1(sz1), a2(sz2);
+  rep(i, sz1) cin >> a1[i];
+  rep(i, sz2) cin >> a2[i];
+
+  vector<ll> sum1, sum2;
+  rep(mask, 1 << sz1)
   {
-    cin >> a[i];
+    ll sum = 0;
+    rep(i, sz1) if (mask>>i&1) sum += a1[i];
+    sum1.emplace_back(sum);
   }
+  rep(mask, 1 << sz2)
+  {
+    ll sum = 0;
+    rep(i, sz2) if (mask>>i&1) sum += a2[i];
+    sum2.emplace_back(sum);
+  }
+
+  sort(ALL(sum2));
+
+  ll ans = sum1[0];
+  for(const auto& v : sum1)
+  {
+    auto itr = upper_bound(ALL(sum2), t - v);
+    if (itr == sum2.begin()) continue;
+    itr--;
+    ll score = v + *itr;
+    if (score <= t) chmax(ans, score);
+    if (score == t) break;
+  }
+  cout << ans << endl;
 }
 
 int main()
