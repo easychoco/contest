2a0a28e6be955d6f471eb7bb1498160a47cbf917
Masashi Okita <easychoco2@gmail.com>
2021-02-21 03:50:41 +0000

    ABC192-F

--- AtCoder/ABC192/F.cpp
+++ AtCoder/ABC192/F.cpp
@@ -31,7 +31,33 @@ void solve()
     cin >> a[i];
   }
 
+  ll ans = 1'000000'000000'000000;
+  const  ll INF = -1'000000'000000'000000;
+  // dp[i][j][m] = i番目まで見てj個使った時に mod k が m のときの最大値
+  repi(k, 1, n + 1)
+  {
+    rep(i1, 110) rep(i2, 110) rep(i3, 110) dp[i1][i2][i3] = INF;
 
+    dp[0][0][0] = 0;
+    dp[0][1][a[0] % k] = a[0];
+    repi(i, 1, n)
+    repi(j, 1, min(i + 2, k + 1))
+    {
+      rep(m, k)
+      {
+        chmax( dp[i][j][m], dp[i - 1][j][m] );
+      }
+      rep(m, k)
+      {
+        if (dp[i - 1][j - 1][m] >= 0)
+        chmax( dp[i][j][(dp[i - 1][j - 1][m] + a[i]) % k], dp[i - 1][j - 1][m] + a[i] );
+      }
+    }
+
+    if (dp[n - 1][k][x % k] > 0)
+    chmin(ans, (x - dp[n - 1][k][x % k]) / k);
+  }
+  show(ans);
 }
 
 int main()
