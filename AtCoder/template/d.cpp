117502695a59073e441266fae7212c4171f2d0db
Masashi Okita <easychoco2@gmail.com>
2021-02-28 20:09:22 +0900

    テンプレにACLを追加

--- AtCoder/template/d.cpp
+++ AtCoder/template/d.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define endl "\n"
@@ -10,6 +11,7 @@
 #define RALL(a) (a).rbegin(),(a).rend()
 
 using namespace std;
+using namespace atcoder;
 using ll = long long;
 using P = pair<ll, ll>;
 void YN(bool a) { cout << (a ? "Yes" : "No") << endl; }
@@ -18,6 +20,7 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true
 void show(){ cout << endl; }
 template <class Head, class... Tail>
 void show(Head&& head, Tail&&... tail){ cout << head << " "; show(std::forward<Tail>(tail)...); }
+template<class T> inline void show(T& begin, T& end) { for(auto itr = begin; itr != end; ++itr) cout<<(*itr)<<" "; cout<<endl; }
 template<class T> inline void showall(T& a) { for(auto v:a) cout<<v<<" "; cout<<endl; }
 
 void solve()
