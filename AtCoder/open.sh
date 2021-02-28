8e75e4e60ed1c1b51fde2098f59f74bfe12a0990
Masashi Okita <easychoco2@gmail.com>
2021-02-28 15:59:09 +0900

    コンテスト用のフォルダを作って開くシェルファイルを作成

---
+++ AtCoder/open.sh
@@ -0,0 +1,14 @@
+if [ ! -e ./$1 ]; then
+  echo $1 is not exist.
+  return
+fi
+ 
+if [ -e ./$1/$2 ]; then
+  echo $2 is already exist.
+else
+  echo create $1/$2
+  cp -r ./template ./$1/$2
+fi
+
+echo open $1/$2
+code ./_tips.cpp ./$1/$2/in ./$1/$2/*.cpp
