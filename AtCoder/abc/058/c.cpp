ff69ce70656c5ba4c0461dbe22ad0fb3466e63e5
Masashi Okita <okita_masashi@uniquevision.co.jp>
2021-03-19 17:14:28 +0900

    ABC058-ABC

---
+++ AtCoder/abc/058/c.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=a;i<(ll)n;++i)
+#define repe(i,n) repie(i,0,n)
+#define repie(i,a,n) for(ll i=a;i<=(ll)n;++i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<ll, ll>;
+void YN(bool a) { cout << (a ? "Yes" : "No") << endl; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+void show(){ cout << endl; }
+template <class Head, class... Tail>
+void show(Head&& head, Tail&&... tail){ cout << head << " "; show(std::forward<Tail>(tail)...); }
+template<class T> inline void showall(T& a) { for(auto v:a) cout<<v<<" "; cout<<endl; }
+
+void solve()
+{
+  ll n;
+  cin >> n;
+  vector<string> s(n);
+  rep(i, n) cin >> s[i];
+
+  vector< vector<ll> > hist(26, vector<ll>(n, 0));
+  rep(i, n)
+  {
+    for(char c : s[i]) hist[(int)(c - 'a')][i]++;
+  }
+
+  string ans = "";
+  rep(i, 26) ans += string(*min_element(ALL(hist[i])), (char)('a' + i));
+  show(ans);
+}
+
+int main()
+{
+  fastio;
+  solve();
+
+  return 0;
+}
