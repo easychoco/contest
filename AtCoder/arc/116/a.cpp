376bfdafaa65c6e3e650b89760ccec1aa99ed398
Masashi Okita <okita_masashi@uniquevision.co.jp>
2021-04-04 11:26:04 +0900

    ARC116-AB

---
+++ AtCoder/arc/116/a.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=a;i<(ll)n;++i)
+#define repe(i,n) repie(i,0,n)
+#define repie(i,a,n) for(ll i=a;i<=(ll)n;++i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<ll, ll>;
+void YN(bool a) { cout << (a ? "Yes" : "No") << endl; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+void show(){ cout << endl; }
+template <class Head, class... Tail>
+void show(Head&& head, Tail&&... tail){ cout << head << " "; show(std::forward<Tail>(tail)...); }
+template<class T> inline void showall(T& a) { for(auto v:a) cout<<v<<" "; cout<<endl; }
+
+void solve()
+{
+  ll t;
+  cin >> t;
+  rep(ti, t)
+  {
+    ll n;
+    cin >> n;
+    if (n % 4 == 0) show("Even");
+    else if (n % 2 == 0) show("Same");
+    else show("Odd");
+  }
+}
+
+int main()
+{
+  fastio;
+  solve();
+
+  return 0;
+}
