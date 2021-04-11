fbb26a56dae7f3a80bbbb518415c1cfa0c4f526b
Masashi Okita <easychoco2@gmail.com>
2021-04-12 00:15:27 +0900

    ABC198参加：3+2完(Rating1334)

---
+++ AtCoder/abc/198/c.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=a;i<(ll)n;++i)
+#define repe(i,n) repie(i,0,n)
+#define repie(i,a,n) for(ll i=a;i<=(ll)n;++i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using P = pair<ll, ll>;
+void YN(bool a) { cout << (a ? "Yes" : "No") << endl; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+void show(){ cout << endl; }
+template <class Head, class... Tail>
+void show(Head&& head, Tail&&... tail){ cout << head << " "; show(std::forward<Tail>(tail)...); }
+template<class T> inline void showall(T& a) { for(auto v:a) cout<<v<<" "; cout<<endl; }
+
+void solve()
+{
+  ll r, x, y;
+  cin >> r >> x >> y;
+
+  ll sum = x*x + y*y;
+  if (sum < r * r)
+  {
+    show(2);
+    return;
+  }
+  rep(i, 300010)
+  {
+    if (i * i >= sum)
+    {
+      // 距離は i-1 ~ i
+      show((i - 1) / r + 1);
+      return;
+    }
+  }
+}
+
+int main()
+{
+  fastio;
+  solve();
+
+  return 0;
+}
