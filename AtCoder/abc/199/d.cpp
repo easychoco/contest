b07e1bc655aff0d3eb3690c38a459433e2ca01eb
Masashi Okita <easychoco2@gmail.com>
2021-04-29 23:45:03 +0900

    ABC199-D

--- AtCoder/abc/199/d.cpp
+++ AtCoder/abc/199/d.cpp
@@ -22,17 +22,67 @@ template <class Head, class... Tail>
 void show(Head&& head, Tail&&... tail){ cout << head << " "; show(std::forward<Tail>(tail)...); }
 template<class T> inline void showall(T& a) { for(auto v:a) cout<<v<<" "; cout<<endl; }
 
+vector< vector<ll> > graph;
+vector<ll> group, color;
+vector<bool> come(20, false);
+ll cnt = 0;
+
+// 連結成分に順番をつける
+void dfs_grp(ll now)
+{
+  if (come[now]) return;
+
+  come[now] = true;
+  group.emplace_back(now);
+
+  for(auto to : graph[now]) dfs_grp(to);
+}
+
+// つけた順番に沿って組み合わせを計算する
+void dfs(ll idx)
+{
+  if (idx == (ll)group.size()) { cnt++; return; }
+  ll now = group[idx];
+
+  repie(c, 0, 2)
+  {
+    color[now] = c;
+    bool ng = false;
+    for(auto to : graph[now]) if (color[now] == color[to]) ng = true;
+    if (ng) continue;
+    dfs(idx + 1);
+  }
+  color[now] = -1;
+}
+
 void solve()
 {
   ll n, m;
   cin >> n >> m;
-  vector<ll> a(n), b(n);
-  rep(i, n)
+  graph = vector<vector<ll>>(n);
+  rep(i, m)
   {
-    cin >> a[i] >> b[i];
+    ll a, b;
+    cin >> a >> b;
+    a--; b--;
+    graph[a].emplace_back(b);
+    graph[b].emplace_back(a);
   }
 
-  
+  ll ans = 1;
+  rep(i, n)
+  {
+    if (come[i]) continue;
+    group.resize(0);
+    dfs_grp(i);
+    ll now = group[0];
+    color = vector<ll>(n, -1);
+    color[now] = 0;
+    cnt = 0;
+    dfs(1);
+    ans *= 3 * cnt;
+  }
+  show(ans);
 }
 
 int main()
