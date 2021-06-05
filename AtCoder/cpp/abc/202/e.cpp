b880821f0ef77ddfd9267a3d481601a5a36b39bc
Masashi Okita <easychoco2@gmail.com>
2021-06-05 18:27:39 +0900

    ABC202-E

--- AtCoder/cpp/abc/202/e.cpp
+++ AtCoder/cpp/abc/202/e.cpp
@@ -22,9 +22,57 @@ template <class Head, class... Tail>
 void show(Head&& head, Tail&&... tail){ cout << head << " "; show(std::forward<Tail>(tail)...); }
 template<class T> inline void showall(T& a) { for(auto v:a) cout<<v<<" "; cout<<endl; }
 
+vector<ll> num(200010, 0);
+vector<ll> ans(200010, 0);
+vector< vector<ll> > graph(200010, vector<ll>());
+vector< vector<tuple<ll, ll>> > query(200010, vector<tuple<ll, ll>>());
+void dfs(ll now, ll depth)
+{
+  for(auto& t : query[now])
+  {
+    ll d, idx;
+    tie(d, idx) = t;
+    ans[idx] = num[d];
+  }
+  num[depth]++;
+
+  for(auto& next : graph[now])
+  {
+    dfs(next, depth + 1);
+  }
+
+  for(auto& t : query[now])
+  {
+    ll d, idx;
+    tie(d, idx) = t;
+    ans[idx] = num[d] - ans[idx];
+  }
+}
+
 void solve()
 {
+  ll n;
+  cin >> n;
+  repi(i, 1, n)
+  {
+    ll p;
+    cin >> p;
+    p--;
+    graph[p].emplace_back(i);
+  }
 
+  ll q;
+  cin >> q;
+  rep(qi, q)
+  {
+    ll u, d;
+    cin >> u >> d;
+    u--;
+    query[u].emplace_back(d, qi);
+  }  
+  
+  dfs(0, 0);
+  rep(i, q) show(ans[i]);
 }
 
 int main()
