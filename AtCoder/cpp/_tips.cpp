546a39375dc7ea64e2140ff2a54299a34a144b5a
Masashi Okita <easychoco2@gmail.com>
2021-06-13 00:12:29 +0900

    tips に強連結成分分解を追加

--- AtCoder/cpp/_tips.cpp
+++ AtCoder/cpp/_tips.cpp
@@ -453,6 +453,73 @@ void tip_topologicalsort_dfs()
   reverse(ALL(sorted));
 }
 
+// SCC : Strongly Connected Component
+// 強連結成分分解 きょうれんけつせいぶんぶんかい
+// O(V + E)
+vector< vector<ll> > graph; // 順方向の
+vector< vector<ll> > graph_rev; // 逆方向
+vector<ll> group; // 各ノードがどのグループに属するか
+vector<ll> seq; // 帰りがけ順の並び
+vector<bool> come;
+vector<ll> hist; // 各グループに属する
+
+// 1回目の DFS
+// 帰りがけ順に記録する
+void scc_dfs(ll now)
+{
+  come[now] = true;
+  for (const auto& next : graph[now])
+  {
+    if (!come[next]) scc_dfs(next);
+  }
+  seq.emplace_back(now);
+}
+
+// 2回目の DFS
+// 記録した番号の大きい順にDFSをして、訪れたノードが強連結成分
+void scc_dfs_rev(ll now, ll grp)
+{
+  come[now] = true;
+  group[now] = grp;
+  hist[grp]++;
+  for (const auto& next : graph_rev[now])
+  {
+    if (!come[next]) scc_dfs_rev(next, grp);
+  }
+}
+
+void tip_scc()
+{
+  ll n, m;
+  cin >> n >> m;
+  graph.resize(n, vector<ll>());
+  graph_rev.resize(n, vector<ll>());
+  group.resize(n, 0);
+  come.resize(n, false);
+  hist.resize(n, 0);
+  rep(i, m)
+  {
+    ll a, b;
+    cin >> a >> b;
+    a--; b--;
+    graph[a].emplace_back(b);
+    graph_rev[b].emplace_back(a);
+  }
+
+  fill(ALL(come), false);
+  rep(i, n)
+  {
+    if (!come[i]) scc_dfs(i);
+  }
+
+  fill(ALL(come), false);
+  ll grp = 0;
+  for(ll i = seq.size() - 1; i >= 0; --i)
+  {
+    if (!come[seq[i]]) scc_dfs_rev(seq[i], grp++);
+  }
+}
+
 
 // =================================================================================================== //
 
