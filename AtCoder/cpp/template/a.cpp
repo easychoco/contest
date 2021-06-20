1ac99d40aa76c0b564bfe4fc602d550dc356e56a
Masashi Okita <easychoco2@gmail.com>
2021-06-21 00:15:40 +0900

    テンプレを変更

--- AtCoder/cpp/template/a.cpp
+++ AtCoder/cpp/template/a.cpp
@@ -4,16 +4,21 @@
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define endl "\n"
 #define rep(i,n) repi(i,0,n)
-#define repi(i,a,n) for(ll i=a;i<(ll)n;++i)
+#define repi(i,a,n) for(ll i=(a);i<(ll)(n);++i)
 #define repe(i,n) repie(i,0,n)
-#define repie(i,a,n) for(ll i=a;i<=(ll)n;++i)
+#define repie(i,a,n) for(ll i=(a);i<=(ll)(n);++i)
+#define rrep(i,n) rrepi(i,n,0)
+#define rrepi(i,n,a) for(ll i=(n);i>=(a);--i)
 #define ALL(a) (a).begin(),(a).end()
 #define RALL(a) (a).rbegin(),(a).rend()
 
 using namespace std;
 using namespace atcoder;
 using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
 using P = pair<ll, ll>;
+const ll INF = 1LL << 60;
 void YN(bool a) { cout << (a ? "Yes" : "No") << endl; }
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
