81034bf2c2096c2b0e6f0e5db044936072bbce93
gummy <easychoco2@gmail.com>
2021-08-14 23:08:05 +0900

    ABC214参加(4完)Rate1434

---
+++ AtCoder/cpp/abc/214/c.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=(a);i<(ll)(n);++i)
+#define repe(i,n) repie(i,0,n)
+#define repie(i,a,n) for(ll i=(a);i<=(ll)(n);++i)
+#define rrep(i,n) rrepi(i,n,0)
+#define rrepi(i,n,a) for(ll i=(n);i>=(a);--i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+using P = pair<ll, ll>;
+const ll INF = 1LL << 60;
+void YN(bool a) { cout << (a ? "Yes" : "No") << endl; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+void show(){ cout << endl; }
+template <class Head, class... Tail>
+void show(Head&& head, Tail&&... tail){ cout << head << " "; show(std::forward<Tail>(tail)...); }
+template<class T> inline void showall(T& a) { for(auto v:a) cout<<v<<" "; cout<<endl; }
+
+void solve()
+{
+  ll n;
+  cin >> n;
+  vector<ll> s(n), t(n);
+  rep(i, n)
+  {
+    cin >> s[i];
+  }
+  rep(i, n)
+  {
+    cin >> t[i];
+  }
+
+  vector<ll> ans(n, INF);
+  priority_queue<P, vector<P>, greater<P>> que;
+  rep(i, n) que.push(P(t[i], i));
+  while(!que.empty())
+  {
+    ll time, idx;
+    tie(time, idx) = que.top();
+    que.pop();
+    if (chmin(ans[idx], time))
+    {
+      que.push(P(time + s[idx], (idx + 1) % n));
+    }
+  }
+  rep(i, n) show(ans[i]);
+}
+
+int main()
+{
+  fastio;
+  solve();
+
+  return 0;
+}
