9a1c71cf633583267c97ef99ff86b3a0b3ac9f85
gummymac <easychoco2@gmail.com>
2022-03-16 01:52:31 +0900

    ARC116-C.cpp

---
+++ AtCoder/cpp/mono/arc116c.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=(a);i<(ll)(n);++i)
+#define repe(i,n) repie(i,0,n)
+#define repie(i,a,n) for(ll i=(a);i<=(ll)(n);++i)
+#define rrep(i,n) rrepi(i,n,0)
+#define rrepi(i,n,a) for(ll i=(n);i>=(a);--i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define pb push_back
+#define eb emplace_back
+
+using namespace std;
+using namespace atcoder;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using ll = int_fast64_t;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using P = pair<ll, ll>;
+using vp = vector<P>;
+using vvp = vector<vp>;
+using T = tuple<ll, ll, ll>;
+using vt = vector<T>;
+const ll INF = 1LL << 60;
+void YN(bool a, string whenT="Yes", string whenF="No") { cout << (a ? whenT : whenF) << endl; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head &&head, Tail &&...tail){ cout << head << " "; print(std::forward<Tail>(tail)...); }
+void printd(double d) { cout << std::fixed << setprecision(13) << d << endl; }
+template <class T>
+inline void printv(T &v){ for (auto a : v) print(a); }
+template <class T>
+inline void printvp(T &p){ for (auto [a, b] : p) print(a, b); }
+template <class T>
+inline void printvt(T &t){ for (auto [a, b, c] : t) print(a, b, c); }
+void debug() { cerr << endl; }
+template <class Head, class... Tail>
+void debug(Head&& head, Tail&&... tail){ cerr << head << " "; debug(std::forward<Tail>(tail)...); }
+
+using mint = modint998244353;
+
+// combination mod prime
+// https://www.youtube.com/watch?v=8uowVvQ_-Mo&feature=youtu.be&t=1619
+struct combination
+{
+  vector<mint> fact, ifact;
+  combination(int n) : fact(n + 1), ifact(n + 1)
+  {
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i)
+      fact[i] = fact[i - 1] * i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i)
+      ifact[i - 1] = ifact[i] * i;
+  }
+  mint operator()(int n, int k)
+  {
+    if (k < 0 || k > n)
+      return 0;
+    return fact[n] * ifact[k] * ifact[n - k];
+  }
+} c(200100);
+
+void solve()
+{
+  ll n, m;
+  cin >> n >> m;
+
+  mint ans = 1;
+  repie(num, 2, m)
+  {
+    map<ll, ll> primes;
+    ll factor = 2;
+    while (factor * factor <= num)
+    {
+      if (num % factor == 0)
+      {
+        primes[factor]++;
+        num /= factor;
+      }
+      else
+        ++factor;
+    }
+    primes[num]++;
+
+    mint cnt = 0;
+    for (auto [k, v] : primes)
+    {
+      cnt *= c(n + v - 1, v);
+    }
+    ans += cnt;
+  }
+
+  print(ans.val());
+
+}
+
+int main()
+{
+  fastio;
+  solve();
+
+  return 0;
+}
