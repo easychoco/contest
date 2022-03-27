1b6397d0fb1d8fed35052e0cd647361857cfa569
gummy <easychoco2@gmail.com>
2022-03-27 11:54:27 +0900

    ABC068-D

--- AtCoder/cpp/abc/068/d.cpp
+++ AtCoder/cpp/abc/068/d.cpp
@@ -1,18 +1,69 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=(a);i<(ll)(n);++i)
+#define repe(i,n) repie(i,0,n)
+#define repie(i,a,n) for(ll i=(a);i<=(ll)(n);++i)
+#define rrep(i,n) rrepi(i,n,0)
+#define rrepi(i,n,a) for(ll i=(n);i>=(a);--i)
 #define ALL(a) (a).begin(),(a).end()
 #define RALL(a) (a).rbegin(),(a).rend()
+#define pb push_back
+#define eb emplace_back
 
 using namespace std;
-using ll = long long;
+using namespace atcoder;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using ll = int_fast64_t;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using P = pair<ll, ll>;
+using vp = vector<P>;
+using vvp = vector<vp>;
+using T = tuple<ll, ll, ll>;
+using vt = vector<T>;
+const ll INF = 1LL << 60;
+void YN(bool a, string whenT="Yes", string whenF="No") { cout << (a ? whenT : whenF) << endl; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head &&head, Tail &&...tail){ cout << head << " "; print(std::forward<Tail>(tail)...); }
+void printd(double d) { cout << std::fixed << setprecision(13) << d << endl; }
+template <class T>
+inline void printv(T &v){ for (auto a : v) print(a); }
+template <class T>
+inline void printvp(T &p){ for (auto [a, b] : p) print(a, b); }
+template <class T>
+inline void printvt(T &t){ for (auto [a, b, c] : t) print(a, b, c); }
+void debug() { cerr << endl; }
+template <class Head, class... Tail>
+void debug(Head&& head, Tail&&... tail){ cerr << head << " "; debug(std::forward<Tail>(tail)...); }
 
-const double PI = 3.14159265358979;
+// 50回やると全体が 1 減る
+// k / 50 * 50 回で 全体を k / 50 減らす
+// k % 50 の端数をいい感じに調整して答え
 
 void solve()
 {
-  
+  ll k;
+  cin >> k;
+
+  vl ans(50, 0);
+  rep(i, k % 50) ans[i] = 50;
+  repi(i, k % 50, 50) ans[i] = 49 - (k % 50);
+
+  rep(i, 50)
+  {
+    ans[i] += k / 50;
+  }
+
+  print(50);
+  printv(ans);
 }
 
 int main()
