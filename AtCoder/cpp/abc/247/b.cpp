8b9951e46b61d779ee264ab5559ef2980d6c1f43
gummy <easychoco2@gmail.com>
2022-04-10 22:49:17 +0900

    ABC246-5完

---
+++ AtCoder/cpp/abc/247/b.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=(a);i<(ll)(n);++i)
+#define repe(i,n) repie(i,0,n)
+#define repie(i,a,n) for(ll i=(a);i<=(ll)(n);++i)
+#define rrep(i,n) rrepi(i,n,0)
+#define rrepi(i,n,a) for(ll i=(n);i>=(a);--i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define pb push_back
+#define eb emplace_back
+
+using namespace std;
+using namespace atcoder;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using ll = int_fast64_t;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using P = pair<string, string>;
+using vp = vector<P>;
+using vvp = vector<vp>;
+using T = tuple<ll, ll, ll>;
+using vt = vector<T>;
+const ll INF = 1LL << 60;
+void YN(bool a, string whenT="Yes", string whenF="No") { cout << (a ? whenT : whenF) << endl; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head &&head, Tail &&...tail){ cout << head << " "; print(std::forward<Tail>(tail)...); }
+void printd(double d) { cout << std::fixed << setprecision(13) << d << endl; }
+template <class T>
+inline void printv(T &v){ for (auto a : v) print(a); }
+template <class T>
+inline void printvp(T &p){ for (auto [a, b] : p) print(a, b); }
+template <class T>
+inline void printvt(T &t){ for (auto [a, b, c] : t) print(a, b, c); }
+void debug() { cerr << endl; }
+template <class Head, class... Tail>
+void debug(Head&& head, Tail&&... tail){ cerr << head << " "; debug(std::forward<Tail>(tail)...); }
+
+void solve()
+{
+  ll n;
+  cin >> n;
+  vp st(n);
+  rep(i, n)
+  {
+    string s, t;
+    cin >> s >> t;
+    st[i] = P(s, t);
+  }
+
+  bool ok = true;
+  rep(i1, n)
+  {
+    auto [s1, t1] = st[i1];
+    bool oks = true;
+    bool okt = true;
+    rep(i2, n)
+    {
+      if (i1 == i2) continue;
+      auto [s2, t2] = st[i2];
+      if (s1 == s2 || s1 == t2) oks &= false;
+    }
+    rep(i2, n)
+    {
+      if (i1 == i2) continue;
+      auto [s2, t2] = st[i2];
+      if (t1 == s2 || t1 == t2) okt &= false;
+    }
+    ok &= (oks | okt);
+  }
+
+  YN(ok);  
+}
+
+int main()
+{
+  fastio;
+  solve();
+
+  return 0;
+}
