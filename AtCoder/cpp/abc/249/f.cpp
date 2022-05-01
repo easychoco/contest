41061544408d4cc87072df08486fbc8c610adf35
gummy <easychoco2@gmail.com>
2022-05-02 03:05:01 +0900

    ABC249-F

--- AtCoder/cpp/abc/249/f.cpp
+++ AtCoder/cpp/abc/249/f.cpp
@@ -8,7 +8,7 @@
 #define repe(i,n) repie(i,0,n)
 #define repie(i,a,n) for(ll i=(a);i<=(ll)(n);++i)
 #define rrep(i,n) rrepi(i,n,0)
-#define rrepi(i,n,a) for(ll i=(n);i>=(a);--i)
+#define rrepi(i,n,a) for(ll i=(ll)(n);i>=(ll)(a);--i)
 #define ALL(a) (a).begin(),(a).end()
 #define RALL(a) (a).rbegin(),(a).rend()
 #define pb push_back
@@ -46,7 +46,76 @@ void debug(Head&& head, Tail&&... tail){ cerr << head << " "; debug(std::forward
 
 void solve()
 {
+  ll n, k;
+  cin >> n >> k;
 
+  vvl ys;
+  ys.pb(vl(1, 0));
+  rep(i, n)
+  {
+    ll t, y;
+    cin >> t >> y;
+    if (t == 1)
+    {
+      ys.pb(vl(1, y));
+    }
+    else
+    {
+      ys.back().pb(y);
+    }
+  }
+
+  priority_queue<ll> max_que; // 除外した整数
+  priority_queue<ll, vl, greater<ll>> min_que; // えらぶ整数
+
+  max_que.push(-INF);
+  min_que.push(INF);
+
+  ll sum = 0;
+  ll ans = -INF;
+  rep(i, ys.size())
+  {
+    if (k - i + 1 < 0) break;
+    // 後ろから ni まで見る
+    ll ni = ys.size() - i - 1;
+    repi(j, 1, ys[ni].size())
+    {
+      ll v = ys[ni][j];
+      max_que.push(v);
+    }
+
+    // 除外したものより小さい数字を選んでいたら、除外済みの最大値と交換する
+    while(min_que.top() < max_que.top())
+    {
+      min_que.push(max_que.top());
+      max_que.push(min_que.top());
+
+      sum += max_que.top();
+      sum -= min_que.top();
+
+      min_que.pop();
+      max_que.pop();
+    }
+
+    // あふれていたら合計が大きくなるように選択する
+    while((ll)max_que.size() > k - i + 1 || max_que.top() > 0)
+    {
+      min_que.push(max_que.top());
+      sum += max_que.top();
+      max_que.pop();      
+    }
+
+    // まだ除外できて、選んだ整数に負の数が含まれていたら除外する
+    while((ll)max_que.size() < k - i + 1 && min_que.top() < 0)
+    {
+      max_que.push(min_que.top());
+      sum -= min_que.top();
+      min_que.pop();
+    }
+
+    chmax(ans, ys[ni][0] + sum);
+  }
+  print(ans);
 }
 
 int main()
