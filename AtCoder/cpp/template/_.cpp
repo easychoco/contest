47f07a9fc32be4f893c947e7082592058f921b22
gummy <easychoco2@gmail.com>
2022-05-02 13:55:13 +0900

    update template

--- AtCoder/cpp/template/_.cpp
+++ AtCoder/cpp/template/_.cpp
@@ -8,7 +8,7 @@
 #define repe(i,n) repie(i,0,n)
 #define repie(i,a,n) for(ll i=(a);i<=(ll)(n);++i)
 #define rrep(i,n) rrepi(i,n,0)
-#define rrepi(i,n,a) for(ll i=(n);i>=(a);--i)
+#define rrepi(i,n,a) for(ll i=(n);i>=(ll)(a);--i)
 #define ALL(a) (a).begin(),(a).end()
 #define RALL(a) (a).rbegin(),(a).rend()
 #define pb push_back
@@ -18,6 +18,7 @@ using namespace std;
 using namespace atcoder;
 using vb = vector<bool>;
 using vvb = vector<vb>;
+using vs = vector<string>;
 using ll = int_fast64_t;
 using vl = vector<ll>;
 using vvl = vector<vl>;
@@ -31,15 +32,17 @@ void YN(bool a, string whenT="Yes", string whenF="No") { cout << (a ? whenT : wh
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 void print() { cout << endl; }
+template <class T>
+void print(T &&t){ cout << t << endl; }
 template <class Head, class... Tail>
 void print(Head &&head, Tail &&...tail){ cout << head << " "; print(std::forward<Tail>(tail)...); }
 void printd(double d) { cout << std::fixed << setprecision(13) << d << endl; }
 template <class T>
 inline void printv(T &v){ for (auto a : v) print(a); }
 template <class T>
-inline void printvp(T &p){ for (auto [a, b] : p) print(a, b); }
+inline void printp(T &p){ for (auto [a, b] : p) print(a, b); }
 template <class T>
-inline void printvt(T &t){ for (auto [a, b, c] : t) print(a, b, c); }
+inline void printt(T &t){ for (auto [a, b, c] : t) print(a, b, c); }
 void debug() { cerr << endl; }
 template <class Head, class... Tail>
 void debug(Head&& head, Tail&&... tail){ cerr << head << " "; debug(std::forward<Tail>(tail)...); }
