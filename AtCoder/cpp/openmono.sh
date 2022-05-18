379bc06a05e72054a0805992bcc6a3d989056705
gummy <easychoco2@gmail.com>
2022-05-19 00:41:39 +0900

    コマンドをちょっと修正

--- AtCoder/cpp/openmono.sh
+++ AtCoder/cpp/openmono.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 if [ -e "mono/${1}.cpp" ]; then
-  echo $1 is already exist.
+  echo "$1" is already exist.
 else
-  echo create mono/$1
+  echo create mono/"$1"
   cp template/_.cpp "mono/${1}.cpp"
 fi
 
-echo opening mono/$1...
+echo opening mono/"$1"...
 code . ./_tips.cpp mono/in "mono/${1}.cpp"
 echo done.
--- AtCoder/python/openmono.sh
+++ AtCoder/python/openmono.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 if [ -e "mono/${1}.py" ]; then
-  echo $1 is already exist.
+  echo "$1" is already exist.
 else
-  echo create mono/$1
+  echo create mono/"$1"
   cp template/_.py "mono/${1}.py"
 fi
 
-echo opening mono/$1.py...
+echo opening mono/"$1".py...
 code . ./_tips.py mono/in "mono/${1}.py"
 echo done.
