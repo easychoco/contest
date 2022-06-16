7b71eba006bf4376d7cc23b1c0d5b1ca9eba0d57
choco <easychoco2@gmail.com>
2022-06-17 02:59:49 +0900

    ABC251-E

---
+++ AtCoder/cpp/abc/251/e.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=(a);i<(ll)(n);++i)
+#define repe(i,n) repie(i,0,n)
+#define repie(i,a,n) for(ll i=(a);i<=(ll)(n);++i)
+#define rrep(i,n) rrepi(i,n,0)
+#define rrepi(i,n,a) for(ll i=(n);i>=(ll)(a);--i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define pb push_back
+#define eb emplace_back
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vs = vector<string>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using P = pair<ll, ll>;
+using vp = vector<P>;
+using vvp = vector<vp>;
+using T = tuple<ll, ll, ll>;
+using vt = vector<T>;
+const ll INF = 1LL << 60;
+void YN(bool a, string whenT="Yes", string whenF="No") { cout << (a ? whenT : whenF) << endl; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+void print() { cout << endl; }
+template <class T>
+void print(T &&t){ cout << t << endl; }
+template <class Head, class... Tail>
+void print(Head &&head, Tail &&...tail){ cout << head << " "; print(std::forward<Tail>(tail)...); }
+void printd(double d) { cout << std::fixed << setprecision(13) << d << endl; }
+template <class T>
+inline void printv(T &v){ for (auto a : v) print(a); }
+template <class T>
+inline void printp(T &p){ for (auto [a, b] : p) print(a, b); }
+template <class T>
+inline void printt(T &t){ for (auto [a, b, c] : t) print(a, b, c); }
+void debug() { cerr << endl; }
+template <class Head, class... Tail>
+void debug(Head&& head, Tail&&... tail){ cerr << head << " "; debug(std::forward<Tail>(tail)...); }
+
+void solve()
+{
+  ll n;
+  cin >> n;
+  vl a(n);
+  rep(i, n) cin >> a[i];
+
+  ll ans = INF;
+  rep(t, 2)
+  {
+    // dp[i][j] = 動物 i まで餌をあげているときの最小コスト
+    // j == 0: A_i をつかっていない
+    // j == 1: A_i をつかった
+    vvl dp(n, vl(2, 0));
+
+    // t == 0: a[0] を使わない
+    // t == 1: a[0] を使う
+    dp[0][1] = a[0];
+    dp[0][t ^ 1] = INF;
+    repi(i, 1, n)
+    {
+      dp[i][0] = dp[i - 1][1];
+      dp[i][1] = min(dp[i - 1][0], dp[i - 1][1]) + a[i];
+    }
+    if (t == 0) chmin(ans, dp[n - 1][1]);
+    else chmin(ans, min(dp[n - 1][0], dp[n - 1][1]));
+  }
+  print(ans);
+}
+
+int main()
+{
+  fastio;
+  solve();
+
+  return 0;
+}
