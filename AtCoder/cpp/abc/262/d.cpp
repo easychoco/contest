06bd7ef021cf40f2c3a0d906f53334847e7368e6
gummy <easychoco2@gmail.com>
2022-07-31 23:51:36 +0900

    ABC262-4完

---
+++ AtCoder/cpp/abc/262/d.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=(a);i<(ll)(n);++i)
+#define repe(i,n) repie(i,0,n)
+#define repie(i,a,n) for(ll i=(a);i<=(ll)(n);++i)
+#define rrep(i,n) rrepi(i,n,0)
+#define rrepi(i,n,a) for(ll i=(n);i>=(ll)(a);--i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define pb push_back
+#define eb emplace_back
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vs = vector<string>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using P = pair<ll, ll>;
+using vp = vector<P>;
+using vvp = vector<vp>;
+using T = tuple<ll, ll, ll>;
+using vt = vector<T>;
+const ll INF = 1LL << 60;
+void YN(bool a, string whenT="Yes", string whenF="No") { cout << (a ? whenT : whenF) << endl; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+void print() { cout << endl; }
+template <class T>
+void print(T &&t){ cout << t << endl; }
+template <class Head, class... Tail>
+void print(Head &&head, Tail &&...tail){ cout << head << " "; print(std::forward<Tail>(tail)...); }
+void printd(double d) { cout << std::fixed << setprecision(13) << d << endl; }
+template <class T>
+inline void printv(T &v){ for (auto a : v) print(a); }
+template <class T>
+inline void printp(T &p){ for (auto [a, b] : p) print(a, b); }
+template <class T>
+inline void printt(T &t){ for (auto [a, b, c] : t) print(a, b, c); }
+void debug() { cerr << endl; }
+template <class Head, class... Tail>
+void debug(Head&& head, Tail&&... tail){ cerr << head << " "; debug(std::forward<Tail>(tail)...); }
+
+using mint = modint998244353;
+
+void solve()
+{
+  ll n;
+  cin >> n;
+  vl a(n);
+  rep(i, n) cin >> a[i];
+
+  // 1個選んだ場合
+  mint ans = n;
+
+  repie(mod, 2, n)
+  {
+    // dp[j][m] = j 個選んで mod が m の個数
+    vector<vector<mint>> dp(mod + 1, vector<mint>(mod, 0));
+    rep(i, n)
+    {
+      rrep(j, mod - 1)
+      rep(m, mod)
+      {
+        // dp[j][m] に a[i] を足す
+        dp[j + 1][(m + a[i]) % mod] += dp[j][m];
+      }
+      dp[1][a[i] % mod] += 1;
+    }
+    ans += dp[mod][0];
+  }
+
+  print(ans.val());
+}
+
+int main()
+{
+  fastio;
+  solve();
+
+  return 0;
+}
