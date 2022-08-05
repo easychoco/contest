1022daf84fc96eab0b5c20e2cc3fca636055c340
choco <easychoco2@gmail.com>
2022-08-05 21:49:10 +0900

    ABC184-D

--- AtCoder/cpp/abc/184/d.cpp
+++ AtCoder/cpp/abc/184/d.cpp
@@ -1,28 +1,71 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define endl "\n"
 #define rep(i,n) repi(i,0,n)
-#define repi(i,a,n) for(ll i=a;i<(ll)n;++i)
+#define repi(i,a,n) for(ll i=(a);i<(ll)(n);++i)
+#define repe(i,n) repie(i,0,n)
+#define repie(i,a,n) for(ll i=(a);i<=(ll)(n);++i)
+#define rrep(i,n) rrepi(i,n,0)
+#define rrepi(i,n,a) for(ll i=(n);i>=(ll)(a);--i)
 #define ALL(a) (a).begin(),(a).end()
 #define RALL(a) (a).rbegin(),(a).rend()
-#define Show(val) cout<<(val)<<" "
-#define Showln(val) cout<<(val)<<endl
+#define pb push_back
+#define eb emplace_back
 
 using namespace std;
+using namespace atcoder;
 using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vs = vector<string>;
+using vl = vector<double>;
+using vvl = vector<vl>;
 using P = pair<ll, ll>;
-// using P = pair<int, int>;
-void YN(bool a) { cout << (a ? "Yes" : "No"); }
+using vp = vector<P>;
+using vvp = vector<vp>;
+using T = tuple<ll, ll, ll>;
+using vt = vector<T>;
+const ll INF = 1LL << 60;
+void YN(bool a, string whenT="Yes", string whenF="No") { cout << (a ? whenT : whenF) << endl; }
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+void print() { cout << endl; }
+template <class T>
+void print(T &&t){ cout << t << endl; }
+template <class Head, class... Tail>
+void print(Head &&head, Tail &&...tail){ cout << head << " "; print(std::forward<Tail>(tail)...); }
+void printd(double d) { cout << std::fixed << setprecision(13) << d << endl; }
+template <class T>
+inline void printv(T &v){ for (auto a : v) print(a); }
+template <class T>
+inline void printp(T &p){ for (auto [a, b] : p) print(a, b); }
+template <class T>
+inline void printt(T &t){ for (auto [a, b, c] : t) print(a, b, c); }
+void debug() { cerr << endl; }
+template <class Head, class... Tail>
+void debug(Head&& head, Tail&&... tail){ cerr << head << " "; debug(std::forward<Tail>(tail)...); }
+
+vector<vvl> memo(101, vvl(101, vl(101, 0.0)));
+
+double f(ll a, ll b, ll c)
+{
+  // memo[a][b][c] = A枚, B枚, C枚のときの答え
+  if (memo[a][b][c] > 0.0) return memo[a][b][c];
+  if (a == 100 || b == 100 || c == 100) return 0.0;
+  double ans = 0.0;
+  ans += (f(a + 1, b, c) + 1.0) * a / (a + b + c);
+  ans += (f(a, b + 1, c) + 1.0) * b / (a + b + c);
+  ans += (f(a, b, c + 1) + 1.0) * c / (a + b + c);
+  return memo[a][b][c] = ans;
+}
 
 void solve()
 {
   ll a, b, c;
   cin >> a >> b >> c;
-
-  
+  printd(f(a, b, c));
 }
 
 int main()
