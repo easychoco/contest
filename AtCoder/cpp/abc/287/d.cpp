7d3b7bdf327249f2023f757a6d7c32556fad30b5
gummy <easychoco2@gmail.com>
2023-01-28 23:00:15 +0900

    ABC287-5完

---
+++ AtCoder/cpp/abc/287/d.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=(a);i<(ll)(n);++i)
+#define repe(i,n) repie(i,0,n)
+#define repie(i,a,n) for(ll i=(a);i<=(ll)(n);++i)
+#define rrep(i,n) rrepi(i,n,0)
+#define rrepi(i,n,a) for(ll i=(n);i>=(ll)(a);--i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define pb push_back
+#define eb emplace_back
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vs = vector<string>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using P = pair<ll, ll>;
+using vp = vector<P>;
+using vvp = vector<vp>;
+using T = tuple<ll, ll, ll>;
+using vt = vector<T>;
+const ll INF = 1LL << 60;
+void YN(bool a, string whenT="Yes", string whenF="No") { cout << (a ? whenT : whenF) << endl; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+void print() { cout << endl; }
+template <class T>
+void print(T &&t){ cout << t << endl; }
+template <class Head, class... Tail>
+void print(Head &&head, Tail &&...tail){ cout << head << " "; print(std::forward<Tail>(tail)...); }
+void printd(double d) { cout << std::fixed << setprecision(13) << d << endl; }
+template <class T>
+inline void printv(T &v){ for (auto a : v) print(a); }
+template <class T>
+inline void printp(T &p){ for (auto [a, b] : p) print(a, b); }
+template <class T>
+inline void printt(T &t){ for (auto [a, b, c] : t) print(a, b, c); }
+void debug() { cerr << endl; }
+template <class Head, class... Tail>
+void debug(Head&& head, Tail&&... tail){ cerr << head << " "; debug(std::forward<Tail>(tail)...); }
+
+void solve()
+{
+  string s, t;
+  cin >> s >> t;
+  ll sn = s.length();
+  ll tn = t.length();
+  string front = "";
+  string back = s.substr(sn - tn);
+
+  // 前からのマッチング情報
+  ll match_front = -1;
+  rep(i, tn)
+  {
+    if (s[i] != '?' && t[i] != '?' && s[i]  != t[i]) break;
+    match_front = i;
+  }
+
+  match_front++;
+
+  // 後からのマッチング情報
+  ll match_back = -1;
+  rep(i, tn)
+  {
+    ll si = sn - 1 - i;
+    ll ti = tn - 1 - i;
+    if (s[si] != '?' && t[ti] != '?' && s[si]  != t[ti]) break;
+    match_back = i;
+  }
+
+  match_back = tn - match_back - 1;
+  repe(i, tn)
+  {
+    YN(i <= match_front && match_back <= i);
+  }
+}
+
+int main()
+{
+  fastio;
+  solve();
+
+  return 0;
+}
