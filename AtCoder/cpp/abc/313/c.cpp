deed25d39f6baca6b6e4914a68f5c1e27def6dcc
gummy <okm.s.choco@gmail.com>
2023-08-26 23:24:54 +0900

    ABC312 ABC313

---
+++ AtCoder/cpp/abc/313/c.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define rep(i,n) repi(i,0,n)
+#define repi(i,a,n) for(ll i=(a);i<(ll)(n);++i)
+#define repe(i,n) repie(i,0,n)
+#define repie(i,a,n) for(ll i=(a);i<=(ll)(n);++i)
+#define rrep(i,n) rrepi(i,n,0)
+#define rrepi(i,n,a) for(ll i=(n);i>=(ll)(a);--i)
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define pb push_back
+#define eb emplace_back
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vs = vector<string>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using P = pair<ll, ll>;
+using vp = vector<P>;
+using vvp = vector<vp>;
+using T = tuple<ll, ll, ll>;
+using vt = vector<T>;
+const ll INF = 1LL << 60;
+void YN(bool a, string whenT="Yes", string whenF="No") { cout << (a ? whenT : whenF) << endl; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+void print() { cout << endl; }
+template <class T>
+void print(T &&t){ cout << t << endl; }
+template <class Head, class... Tail>
+void print(Head &&head, Tail &&...tail){ cout << head << " "; print(std::forward<Tail>(tail)...); }
+void printd(double d) { cout << std::fixed << setprecision(13) << d << endl; }
+template <class T>
+inline void printv(T &v){ for (auto a : v) print(a); }
+template <class T>
+inline void printp(T &p){ for (auto [a, b] : p) print(a, b); }
+template <class T>
+inline void printt(T &t){ for (auto [a, b, c] : t) print(a, b, c); }
+void debug() { cerr << endl; }
+template <class Head, class... Tail>
+void debug(Head&& head, Tail&&... tail){ cerr << head << " "; debug(std::forward<Tail>(tail)...); }
+
+void solve()
+{
+  ll n;
+  cin >> n;
+  vl a(n);
+  rep(i, n) cin >> a[i];
+
+  ll sum = accumulate(ALL(a), 0LL);
+  ll avg = sum / n;
+  ll n_avg = n * avg;
+  
+  // avg     が n - v 個
+  // avg + 1 が     v 個
+  // avg * (n - v) + (avg + 1) * v = sum
+  ll v = sum - n_avg;
+
+  // 前半の n - v 個を avg にして
+  // 後半の v 個を avg + 1 にすれば勝ち
+  sort(ALL(a));
+  ll diff = 0;
+
+  // 前半
+  rep(i, n - v)
+  {
+    diff += abs(a[i] - avg);
+  }
+
+  // 後半
+  repi(i, n - v, n)
+  {
+    diff += abs(a[i] - avg - 1);
+  }
+  print(diff / 2);
+}
+
+int main()
+{
+  fastio;
+  solve();
+
+  return 0;
+}
